a80769b7418a3ddfcc5f87d8c9fa6eedc25e42ec
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2024-05-30 20:21:00 +0600

    Folder model

---
+++ lib/models/folder.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+import 'package:ltr/models/subject.dart';
+
+part 'folder.g.dart';
+
+@HiveType(typeId: 0)
+class Folder extends HiveObject {
+  @HiveField(0)
+  late String name;
+
+  @HiveField(1)
+  late String uuid;
+
+  // Store the owner as a Hive object
+  @HiveField(2)
+  Subject? subject;
+  // Subject(this.name);
+
+  @HiveField(3)
+  Folder? parentFolder;
+}
